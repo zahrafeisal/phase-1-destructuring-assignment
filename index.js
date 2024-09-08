1399d0ccf77b795ba26d41c14e180d8fb713a855
zahrafeisal <zahrafeisal3@gmail.com>
2024-09-08 19:48:30 +0300

    destructuring assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink,] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] =  colors;
+const [r, o, y, g, b, ,v] = colors;
+const [, , , , , indg,] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +15,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName, color, song, job, partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +32,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {nestedJob, nestedPartner} =  nestedMuppet;
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
 
 // Strings
 
